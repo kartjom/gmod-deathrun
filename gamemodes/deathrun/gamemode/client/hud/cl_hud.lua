66aee781db3528c60d092ae342f0b4a01c24d8d7
kartjom <jakubrutkowski3012@gmail.com>
2024-01-22 21:24:28 +0100

    spectator hints

--- gamemodes/deathrun/gamemode/client/hud/cl_hud.lua
+++ gamemodes/deathrun/gamemode/client/hud/cl_hud.lua
@@ -27,3 +27,11 @@ hook.Add("HUDPaint", "HUD_DrawRoundTime", function()
 
     draw.DrawText(text, "Trebuchet24", x, y, Color(255, 255, 255, 255), align);
 end)
+
+hook.Add("HUDPaint", "HUD_SpectatorHints", function()
+    if (LocalPlayer():Team() == TEAM.SPECTATOR) then
+        draw.DrawText("- Press [Left Mouse] to follow next player", "Trebuchet24", (ScrW()/2) - 200, ScrH() - 80, Color(255, 255, 255, 128), TEXT_ALIGN_LEFT)
+        draw.DrawText("- Press [Right Mouse] to follow previous player", "Trebuchet24", (ScrW()/2) - 200, ScrH() - 60, Color(255, 255, 255, 128), TEXT_ALIGN_LEFT)
+        draw.DrawText("- Press [R] to change camera mode", "Trebuchet24", (ScrW()/2) - 200, ScrH() - 40, Color(255, 255, 255, 128), TEXT_ALIGN_LEFT)
+    end
+end)
