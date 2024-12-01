aaec2543e7f8c9d14f737115d4aebeadbabf232f
kartjom <jakubrutkowski3012@gmail.com>
2024-12-01 14:49:48 +0100

    game_round_win update

--- gamemodes/deathrun/entities/entities/game_round_win.lua
+++ gamemodes/deathrun/entities/entities/game_round_win.lua
@@ -1,9 +1,28 @@
 ENT.Base = "base_point"
 ENT.Type = "point"
 
-function ENT:AcceptInput(inputName, activator, caller, data)
-    if ( string.iequals(inputName, "RoundWin") ) then
-        local winTeam = self:GetInternalVariable("TeamNum")
-        DEATHRUN.RoundManager.EndRound(winTeam, team.GetName(winTeam).." win!")
+function ENT:KeyValue(key, value)
+    if (string.StartsWith(key, "On")) then
+		self:StoreOutput(key, value)
+	else
+        self:StoreValue(key, value)
     end
+end
+
+function ENT:AcceptInput(inputName, activator, caller, data)
+    if (string.iequals(inputName, "RoundWin")) then self:RoundWin() end
+    if (string.iequals(inputName, "SetTeam")) then self:SetTeam( tonumber(data) ) return end
+end
+
+function ENT:Initialize()
+    self.TeamNum = self:GetStoredValue("TeamNum", "int", 0)
+end
+
+function ENT:RoundWin()
+    DEATHRUN.RoundManager.EndRound(self.TeamNum, team.GetName(self.TeamNum).." win!")
+    self:TriggerOutput("OnRoundWin")
+end
+
+function ENT:SetTeam(num)
+    self.TeamNum = num
 end
